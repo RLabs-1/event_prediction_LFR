adcb495df62e5ae675df603b7d6c61dfeef24b53
Abdelhadi Heib <hadiheib99@gmail.com>
2025-04-17 19:19:42 +0300

    modified log_praser

--- log_processor/oneTest.py
+++
@@ -1,5 +0,0 @@
-from log_processor.log_parser import LogParser
-
-parser = LogParser()
-parsed_logs = parser.parse_log("2024-03-27 12:00:00 ERROR MainClass - Something went wrong")
-print(parsed_logs)
